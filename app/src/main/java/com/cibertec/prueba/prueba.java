2a1118fce81057436640c8c3d0772ca9b8d0e63e
jluistorres <jtorreszarate2405@gmail.com>
2016-05-16 16:09:29 -0500

    prueba

--- app/src/main/java/com/cibertec/prueba/prueba.java
+++ app/src/main/java/com/cibertec/prueba/prueba.java
@@ -4,4 +4,22 @@ package com.cibertec.prueba;
  * Created by smachado on 16/05/2016.
  */
 public class prueba {
+    private int id;
+    private String texto;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 }
